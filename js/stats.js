2ae78b178cea9484e1fb2f8130577c2a13448012
Nicolas Reinhard <nicolasreinhard.pro@gmail.com>
2020-03-18 18:10:32 +0100

    [Stats Update]
    Changed PNL Stats

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,7 @@
 'use strict'
 const api = require('./api-futures')
 
-module.exports = { getPNL }
+module.exports = { getPNL, getDailyPNL }
 
 function getPNL () {
     if (!api.positions[0] || api.positions[0].qty == 0)
@@ -12,9 +12,32 @@ function getPNL () {
 
     var pnl = (price - entryPrice) / entryPrice * qty * price
     var pnlPercent = pnl / api.account.totalWalletBalance
-    return pnl
+    return { pnl: pnl, percent: pnlPercent }
+
 }
 
 function getBreakEven () {
 
+}
+
+
+async function getDailyPNL(){
+    var incomeHistory
+    var currentBalance = +api.account.totalWalletBalance
+
+    var startTime = new Date().setHours(4)
+    await api.binance.futuresIncome({symbol: SYMBOL, startTime: startTime, endTime: Date.now()})
+        .then(response => incomeHistory = response)
+    var PNLarray = incomeHistory.filter(x => x.incomeType == "REALIZED_PNL" && x.symbol == SYMBOL)
+
+    var total = 0
+    for (let x of PNLarray){
+        total += +x.income
+    }
+    var oldBalance = currentBalance - total
+
+    return { pnl: total, percent: (total / oldBalance) }
+
+
+    "BTCUSDT", {orderId: "1025137386"}
 }
