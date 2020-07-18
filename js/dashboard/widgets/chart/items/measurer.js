e38483ad8d8f1069e1776b2bbb5d99922c477a20
Xodetaetl <dev@xod.me>
2020-07-18 16:55:11 +0200

    Measurer: remove code for seconds

--- js/dashboard/widgets/chart/items/measurer.js
+++ js/dashboard/widgets/chart/items/measurer.js
@@ -124,20 +124,18 @@ module.exports = class Measurer {
     }
 
     _getTimeInterval (milliseconds) {
-        let days, hours, minutes, seconds, total_hours, total_minutes, total_seconds
+        let days, hours, minutes, total_hours, total_minutes, total_seconds
 
         total_seconds = parseInt(Math.floor(milliseconds / 1000))
         total_minutes = parseInt(Math.floor(total_seconds / 60))
         total_hours = parseInt(Math.floor(total_minutes / 60))
         days = parseInt(Math.floor(total_hours / 24))
 
-        seconds = parseInt(total_seconds % 60)
         minutes = parseInt(total_minutes % 60)
         hours = parseInt(total_hours % 24)
 
         return (days ? days + 'd ' : '')
             + (hours ? hours + 'h ' : '')
             + (minutes ? minutes + 'm' : '')
-            // + (seconds ? seconds + 's ' : '')
     }
 }
