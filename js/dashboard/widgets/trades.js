323e38f2f468248cb4af8c1ae12d57522e895118
Xodetaetl <dev@xod.me>
2020-08-21 12:33:22 +0200

    Polish Trades widget

--- js/dashboard/widgets/trades.js
+++ js/dashboard/widgets/trades.js
@@ -2,33 +2,43 @@
 events.on('api.newTrade', updateTrades)
 
 let data = []
-let table = d3.select('#trades').append('div')
-        .class('table')
+let table = d3.select('#trades').append('table')
 
-let timer = 0
+
+table.append('tr').selectAll('th')
+    .data(['Price', 'Amount', '₮ Value'])
+    .enter().append('th')
+        .html(d => d)
+
 function updateTrades (d) {
+    d.value = d.price * d.amount
+
+    if (d.value < 10)
+        return // Hide small trades under $10
+
+    d.value = (d.value >= 10000)
+            ? nFormat('.2s', d.value)
+            : nFormat(',d', d.value)
+
     data.unshift(d)
     if (data.length > 30) data.pop()
 
-    if (Date.now() < timer + 16) return // throttle redraw to 60 fps
-    timer = Date.now()
-
-    table.selectAll('.table > div')
+    table.selectAll('tr:not(:first-child)')
         .data(data)
         .join(
-            enter => enter.append('div')
-                .class(d => 'row ' + ((d.maker) ? 'sell' : 'buy'))
+            enter => enter.append('tr')
+                .class(d => ((d.maker) ? 'sell' : 'buy'))
                 .call(row => {
-                    row.selectAll('div')
-                        .data(d => [d.price, d.amount])
-                        .enter().append('div')
+                    row.selectAll('td')
+                        .data(d => [d.price, d.amount, d.value])
+                        .enter().append('td')
                             .html(d => d)
             }),
             update => update
                 .call(row => {
-                    row.class(d => 'row ' + ((d.maker) ? 'sell' : 'buy'))
-                    row.selectAll('div')
-                        .data(d => [d.price, d.amount])
+                    row.class(d => ((d.maker) ? 'sell' : 'buy'))
+                    row.selectAll('td')
+                        .data(d => [d.price, d.amount, d.value])
                             .html(d => d)
             })
     )
