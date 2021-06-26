757ed44b163ac513d949ec39d2da8c21bb9ffabf
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-06-26 18:40:26 +0300

    chore: Minor

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 export { default } from './Root';
 
 // types for biduul-types
-export * from './api';
-export * from './store';
+export * from './api/types';
+export * from './store/types';
+export { RootStore } from './store';
