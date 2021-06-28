8d6472e655174d53a3ecd3d036083202b1605767
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-06-28 18:41:52 +0300

    feat: Plugins implementation update (before biduul-types publish)

--- app/store/global.d.ts
+++ app/store/global.d.ts
@@ -1,7 +1,8 @@
-import { RootStore } from '.';
+/* import { RootStore } from '.';
 
 declare global {
   // this global type is created to avoid circular references at store branches
   // (Market, PersistentStore etc) to make them use RootStore safely
   type Store = RootStore;
 }
+ */
