6cc94ef478da21fe48c2d008b377d92b3f66ed03
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-07-06 21:44:21 +0300

    fix: Switch Market and Limit tabs

--- app/components/widgets/TradingWidget/index.tsx
+++ app/components/widgets/TradingWidget/index.tsx
@@ -29,27 +29,19 @@ const Trading = ({ title, id }: { title: string; id: string; }): ReactElement =>
       />
       <Nav tabs className="mt-3">
         <NavItem>
-          <NavLink active={type === 'MARKET'} onClick={() => setType('MARKET')} className="cursor-pointer">Market</NavLink>
-        </NavItem>
-        <NavItem>
           <NavLink active={type === 'LIMIT'} onClick={() => setType('LIMIT')} className="cursor-pointer">Limit</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink active={type === 'STOP_MARKET'} onClick={() => setType('STOP_MARKET')} className="cursor-pointer">Stop Market</NavLink>
+          <NavLink active={type === 'MARKET'} onClick={() => setType('MARKET')} className="cursor-pointer">Market</NavLink>
         </NavItem>
         <NavItem>
           <NavLink active={type === 'STOP'} onClick={() => setType('STOP')} className="cursor-pointer">Stop Limit</NavLink>
         </NavItem>
+        <NavItem>
+          <NavLink active={type === 'STOP_MARKET'} onClick={() => setType('STOP_MARKET')} className="cursor-pointer">Stop Market</NavLink>
+        </NavItem>
       </Nav>
       <TabContent className="p-1" data-trading-type={type}>
-        <div className={`tab-pane fade${type === 'MARKET' ? ' show active' : ''}`}>
-          <Market
-            isWideLayout={isWideLayout}
-            postOnly={postOnly}
-            reduceOnly={reduceOnly}
-            tradingType="MARKET"
-          />
-        </div>
         <div className={`tab-pane fade${type === 'LIMIT' ? ' show active' : ''}`}>
           <Limit
             isWideLayout={isWideLayout}
@@ -58,12 +50,12 @@ const Trading = ({ title, id }: { title: string; id: string; }): ReactElement =>
             tradingType="LIMIT"
           />
         </div>
-        <div className={`tab-pane fade${type === 'STOP_MARKET' ? ' show active' : ''}`}>
-          <StopMarket
+        <div className={`tab-pane fade${type === 'MARKET' ? ' show active' : ''}`}>
+          <Market
             isWideLayout={isWideLayout}
             postOnly={postOnly}
             reduceOnly={reduceOnly}
-            tradingType="STOP_MARKET"
+            tradingType="MARKET"
           />
         </div>
         <div className={`tab-pane fade${type === 'STOP' ? ' show active' : ''}`}>
@@ -72,6 +64,14 @@ const Trading = ({ title, id }: { title: string; id: string; }): ReactElement =>
             postOnly={postOnly}
             reduceOnly={reduceOnly}
             tradingType="STOP"
+          />
+        </div>
+        <div className={`tab-pane fade${type === 'STOP_MARKET' ? ' show active' : ''}`}>
+          <StopMarket
+            isWideLayout={isWideLayout}
+            postOnly={postOnly}
+            reduceOnly={reduceOnly}
+            tradingType="STOP_MARKET"
           />
         </div>
       </TabContent>
