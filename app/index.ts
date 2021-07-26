fd4ccc0aee418b86e7bd70f492340a623e29997b
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-07-26 20:56:49 +0300

    ci: Fix test error

--- app/index.ts
+++ app/index.ts
@@ -3,4 +3,4 @@ export { default } from './Root';
 // types for biduul-types
 export * from './api/types';
 export * from './store/types';
-export { RootStore, Foo } from './store';
+export { RootStore } from './store';
