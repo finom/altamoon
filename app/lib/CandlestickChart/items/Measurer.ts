6aad1e83069d059a4b989973ce346d16b39126d3
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-08-05 22:21:18 +0300

    fix: Measurer width fix

--- app/lib/CandlestickChart/items/Measurer.ts
+++ app/lib/CandlestickChart/items/Measurer.ts
@@ -174,12 +174,12 @@ export default class Measurer {
     const orderLeveragedPercent = ((y2 - y1) / y1) * orderLeverage;
 
     this.#label?.html(`
-      <b>${time}</b><br>
-      <b>${nFormat(',.2f', amount)}</b> USDT<br>
-      <b>${nFormat('+,.2%', percentage)}</b><br>
-      <b>${nFormat('+,.1%', leveragedPercent)}</b> at ${this.#currentSymbolLeverage}x<br>
-      <b>${nFormat('+,.1%', trueLeveragedPercent)}</b> at ${Measurer.formatLeverage(trueLeverage)}x (position)<br>
-      <b>${nFormat('+,.1%', orderLeveragedPercent)}</b> at ${Measurer.formatLeverage(orderLeverage)}x (${side.toLowerCase()} qty)
+      <p><b>${time}</b></p>
+      <p><b>${nFormat(',.2f', amount)}</b> USDT</p>
+      <p><b>${nFormat('+,.2%', percentage)}</b></p>
+      <p><b>${nFormat('+,.1%', leveragedPercent)}</b> at ${this.#currentSymbolLeverage}x</p>
+      <p><b>${nFormat('+,.1%', trueLeveragedPercent)}</b> at ${Measurer.formatLeverage(trueLeverage)}x (position)</p>
+      <p><b>${nFormat('+,.1%', orderLeveragedPercent)}</b> at ${Measurer.formatLeverage(orderLeverage)}x (${side.toLowerCase()} qty)</p>
     `);
   }
 
