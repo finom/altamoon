3c2d76d44814ac16d45248ea187cc0abb91b5d72
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-09-13 22:22:06 +0300

    chore: Fix start script for browser version

--- .erb/configs/webpack.config.browser.dev.babel.js
+++ .erb/configs/webpack.config.browser.dev.babel.js
@@ -1,5 +1,7 @@
 import webpack from 'webpack';
 import { merge } from 'webpack-merge';
+import CopyPlugin from "copy-webpack-plugin";
+
 
 import baseConfig from './webpack.config.base';
 import CheckNodeEnv from '../scripts/CheckNodeEnv';
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const port = process.env.PORT || 1213;
-const publicPath = `http://localhost:${port}/dist`;
+const publicPath = `http://localhost:${port}/`;
 
 export default merge(baseConfig, {
   devtool: 'inline-source-map',
@@ -50,14 +52,6 @@ export default merge(baseConfig, {
           },
         ],
       },
-      /* {
-        test: /\.theme\.css$/,
-        use: [
-          {
-            loader: 'raw-loader',
-          },
-        ],
-      }, */
       {
         test: /\.global\.css$/,
         use: [
@@ -188,8 +182,13 @@ export default merge(baseConfig, {
     }),
 
     new ReactRefreshWebpackPlugin(),
-  ],
 
+    new CopyPlugin({
+      patterns: [
+        { from: "src/browser.html", to: "" },
+      ],
+    }),
+  ],
   node: {
     __dirname: false,
     __filename: false,
@@ -197,23 +196,9 @@ export default merge(baseConfig, {
 
   devServer: {
     port,
-    publicPath,
     compress: true,
-    noInfo: false,
-    stats: 'errors-only',
-    inline: true,
-    lazy: false,
     hot: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    watchOptions: {
-      aggregateTimeout: 300,
-      ignored: /node_modules/,
-      poll: 100,
-    },
-    historyApiFallback: {
-      verbose: true,
-      disableDotRule: false,
-      index: 'src/browser.html',
-    },
+
   },
 });
