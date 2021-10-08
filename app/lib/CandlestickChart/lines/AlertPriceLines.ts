43fa2c21f36cb8d6ac4e810343fc791bf751994f
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-10-08 21:09:13 +0200

    fix: Use bell icon at alert line

--- app/lib/CandlestickChart/lines/AlertPriceLines.ts
+++ app/lib/CandlestickChart/lines/AlertPriceLines.ts
@@ -29,7 +29,9 @@ export default class AlertPriceLines extends PriceLines {
 
   private static createAlertLine = (yValue: number): AlertLinesDatum => ({
     yValue,
-    title: 'Alert',
+    title: `<svg style="transform: scale(0.7) translate(0, -3px);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
+    </svg>`,
     isDraggable: true,
     customData: {},
     color: this.color,
