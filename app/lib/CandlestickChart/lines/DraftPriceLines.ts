971563cfce3d716441d1dfa3e162ffb9510903a9
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-10-24 20:11:57 +0200

    fix: Deps cycle

--- app/lib/CandlestickChart/lines/DraftPriceLines.ts
+++ app/lib/CandlestickChart/lines/DraftPriceLines.ts
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
-import { RootStore } from '../../..';
+import { RootStore } from '../../../store';
 import { OrderSide } from '../../../api';
 import formatMoneyNumber from '../../formatMoneyNumber';
 import {
