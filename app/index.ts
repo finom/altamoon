3312adad9eb5789f9e41378df94f465dd8b33734
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-11-20 19:37:41 +0200

    fix: Ecmascript error

--- app/index.ts
+++ app/index.ts
@@ -5,5 +5,5 @@ export * from './api/types';
 export * from './store/types';
 export { RootStore } from './store';
 
-const logo =  document.querySelector<HTMLImageElement>('#loading-logo');
+const logo = document.querySelector<HTMLImageElement>('#loading-logo');
 if (logo) logo.style.display = 'none';
