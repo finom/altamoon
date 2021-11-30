f3d1f5efd55f2417020f94f9ffd640c9811a5251
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2021-11-30 22:26:51 +0200

    chore: Add API docs (testing how it looks on Github)

--- app/api/futuresREST.ts
+++ app/api/futuresREST.ts
@@ -12,9 +12,11 @@ export { futuresCandles };
 /**
  * Array of all possible intervals.
  * @example
+ * ```ts
  * import { futuresIntervals } from 'altamoon-binance-api';
  *
  * console.log(futuresIntervals);
+ * ```
  */
 export const futuresIntervals: CandlestickChartInterval[] = [
   '1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M',
