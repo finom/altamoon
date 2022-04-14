25e018932ca7795c80c3a2df384c3e4c7a578cb4
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2022-04-14 20:39:41 +0200

    fix: Use new React API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from '../app';
 
-render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root') as Element).render(<App />);
